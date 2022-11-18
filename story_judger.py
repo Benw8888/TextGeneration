473b3f2f572492becf527c72992514566ff49a72
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2022-11-18 01:03:48 -0500

    Update story_judger.py

--- story_judger.py
+++ story_judger.py
@@ -1,3 +1,27 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e590046ea76ba2bee0d41d0fffa2cdfe1dc20c6f378f95824dcf1ef4b931a15a
-size 256
+import numpy as np
+import torch
+import torch.nn as nn
+
+# Implement story judger
+
+
+class StoryJudger(nn.Module):
+    """
+    Try to make a baseline story-judger using an LSTM.
+    """
+    def __init__(self):
+        super().__init__()
+        # define word embedding module:
+        embedding_dim = 512
+        hidden_dim = 1000
+        self.embedding = nn.Embedding(num_embeddings=50257,embedding_dim=embedding_dim, dtype=torch.float64)
+        # define lstm module:
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=2, bidirectional=False, dtype=torch.float64, batch_first=True)
+        self.linear = nn.Linear(hidden_dim,1, dtype=torch.float64)
+
+    def forward(self,x):
+        embedded_x = self.embedding(x)
+
+        lstm_outputs, _ = self.lstm(embedded_x.to(torch.float64))
+        return self.linear(lstm_outputs[:,-1])
+        pass

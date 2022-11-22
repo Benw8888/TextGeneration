460a4397bef0e5a58eeea136f3031e8c609e226a
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2022-11-21 23:07:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -38,34 +38,35 @@ if __name__ == '__main__':
     with gzip.open('dataset_list_cache.p', 'rb') as inp:
         full_dataset = pickle.load(inp)
 
-    wrap_dataset = WrapperDataset.WrapperDataset(full_dataset, device=device)
+    wrap_dataset = WrapperDataset.WrapperDataset(full_dataset, only_data=True, device=device)
 
     wrap_dataset.dataset.update_story_ids()
 
     total_dataset_length = len(wrap_dataset)
     print("TOTAL DATASET LENGTH: ", total_dataset_length)
-    train_dataset_length = 20000
-    test_dataset_length = 2000
-    remainder = total_dataset_length - train_dataset_length - test_dataset_length
-    train_dataset, test_dataset, _ = random_split(wrap_dataset,[train_dataset_length,test_dataset_length, remainder])
-    train_dataloader = torch.utils.data.DataLoader(train_dataset,batch_size = 64, num_workers=16,shuffle=True)
-    test_dataloader = torch.utils.data.DataLoader(test_dataset,batch_size = 1, num_workers=16,)
 
-    # CREATE MODEL
 
-    baseline_pretrained = True
-    if baseline_pretrained:
-        with gzip.open('baseline_judger.p', 'rb') as inp:
-            baseline_judger = pickle.load(inp)
-            baseline_judger.lstm.flatten_parameters()
-    else:
-        baseline_judger = story_judger.StoryJudger().to(device)
-
-    loss_fn = torch.nn.functional.mse_loss
-
-    # TRAIN MODEL
+    # TRAIN JUDGER MODEL:
     if False:
-        optimizer = torch.optim.Adam(baseline_judger.parameters(),lr=0.001,)
+        train_dataset_length = 20000
+        test_dataset_length = 2000
+        remainder = total_dataset_length - train_dataset_length - test_dataset_length
+        train_dataset, test_dataset, _ = random_split(wrap_dataset,
+                                                      [train_dataset_length, test_dataset_length, remainder])
+        train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=64, num_workers=16, shuffle=True)
+        test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=1, num_workers=16, )
+
+        # CREATE MODEL
+
+        baseline_pretrained = False
+        if baseline_pretrained:
+            with gzip.open('baseline_judger.p', 'rb') as inp:
+                baseline_judger = pickle.load(inp)
+                baseline_judger.lstm.flatten_parameters()
+        else:
+            baseline_judger = story_judger.StoryJudger().to(device)
+
+        loss_fn = torch.nn.functional.mse_loss
 
         num_epochs = 5
         for e in (range(num_epochs)):
@@ -83,40 +84,44 @@ if __name__ == '__main__':
                     print("CURRENT LOSS: ", total_loss.item()/50)
                     total_loss = 0
 
-        print("SAVING JUDGER MODEL: ")
+            print("SAVING JUDGER MODEL: ")
 
-        compress = True
-        if compress:
-            open_func = gzip.open
-        else:
-            open_func = open
-        with open_func("baseline_judger.p", 'wb') as outp:
-            pickle.dump(baseline_judger, outp, -1)
-
-    print("CALCULATING TEST LOSS: ")
-
-    with torch.no_grad():
-        total_loss = 0
-        cur_step = 0
-        for step, (x, y) in enumerate(tqdm(test_dataloader, position=0, leave=True)):
-            output_pred = baseline_judger(x.to(device))
-            loss = loss_fn(output_pred, y.to(device))
-            total_loss += torch.mean(loss)
-            cur_step = step
-
-        print("num steps: ", cur_step+1)
-        print("TEST LOSS: ", total_loss.item() / (cur_step+1))
+            compress = True
+            if compress:
+                open_func = gzip.open
+            else:
+                open_func = open
+            with open_func("baseline_judger.p", 'wb') as outp:
+                pickle.dump(baseline_judger, outp, -1)
 
 
-    exit()
+        print("examining baseline judger: ")
+
+
+        print("CALCULATING TEST LOSS: ")
+
+        with torch.no_grad():
+            total_loss = 0
+            cur_step = 0
+            for step, (x, y) in enumerate(tqdm(test_dataloader, position=0, leave=True)):
+                output_pred = baseline_judger(x.to(device))
+                loss = loss_fn(output_pred, y.to(device))
+                total_loss += torch.mean(loss)
+                cur_step = step
+
+            print("num steps: ", cur_step+1)
+            print("TEST LOSS: ", total_loss.item() / (cur_step+1))
+
+
+        exit()
 
     #file_name = "fanfics.csv_text_files/94746.txt"
     #new_story = TokenDataset(file_name, tokenizer=tokenizer, padding_side="left")
 
     # create default ai text gen model
-    load_existing = False # turn to true once we already have a model stored on file
+    load_existing = True # turn to true once we already have a model stored on file
     if load_existing:
-        ai = aitextgen(model_folder="trained_model2")
+        ai = aitextgen(model_folder="trained_model4")
     else:
         ai = aitextgen()
 
@@ -125,18 +130,13 @@ if __name__ == '__main__':
     #print(len(tokenizer.decode(full_dataset.tokens)))
 
     # Train the model! It will save pytorch_model.bin periodically and after completion to the `trained_model` folder.
-    ai.train(full_dataset, output_dir="trained_model3",batch_size=8, freeze_layers=4, num_steps=100, generate_every=10, save_every=1000, padding_side="left")
+    # alreay trained: 6560, 43440
+    ai.train(wrap_dataset, output_dir="trained_model4",batch_size=8, freeze_layers=True, num_layers_freeze=4, num_steps=50000, generate_every=1000, save_every=500, padding_side="right")
 
     # Generate text from it!
-    ai.generate(2, prompt="Naruto formed his hands into a cross shape. \"Shadow Clone Jutsu!\" he yelled, and in a burst of smoke a hundred copies of him appeared.\n\n"
-                           "Sasuke looked at him in disdain, grotesque hands growing out of his back from the effects of Orochimaru's curse mark. \"You can't stop me. I'll do anything I can to get the power to kill my brother Itachi. Even if that means turning to Orochimaru.\"\n\n"
-                           "Naruto grit his teeth in determination. \"I'll stop you! I'll bring you back to Konoha for sure!\"" )
+    #ai.generate(10)
 
     #cuda.cudaDeviceReset()
-
-    # With your trained model, you can reload the model at any time by
-    # providing the folder containing the pytorch_model.bin model weights + the config, and providing the tokenizer.
-    #ai2 = aitextgen(model_folder="trained_model")
 
     #ai2.generate(10, prompt="ROMEO:")
 

fe4b24ee463c731015d2425ad5d0e1933b13fa9c
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2022-11-28 18:26:06 -0500

    Update AutoEncoder.py

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -2,41 +2,38 @@ import numpy as np
 import torch
 import torch.nn as nn
 import pytorch_lightning as pl
+from torch.utils.data import DataLoader
 import Encoder
 import Decoder
 
 # Implement autoencoder, combining both encoder and decoder
 
-class AutoEncoder(pl.LightningModule):
+class AutoEncoder(nn.Module):
     """
     Try to make an LSTM encoder
     """
-    def __init__(self, level="paragraph"):
+    def __init__(self):
         super().__init__()
         # define word embedding module:
         self.encoder = Encoder.Encoder()
         self.decoder = Decoder.Decoder()
-
-        embedding_dim = 512
-        hidden_dim = 768
-        output_dim = 768
-        self.embedding = nn.Embedding(num_embeddings=50257,embedding_dim=embedding_dim, dtype=torch.float64)
-        # define lstm module:
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=4, bidirectional=True, dtype=torch.float64, batch_first=True)
-        self.linear = nn.Linear(hidden_dim*2,output_dim, dtype=torch.float64)
+        self.decoder_word_embeddings = self.decoder.word_embeddings
 
     def forward(self,x):
-        embedded_x = self.embedding(x)
-        lstm_outputs, _ = self.lstm(embedded_x.to(torch.float64))
-        return self.linear(lstm_outputs[:,-1])
+        paragraph_embedding = self.encoder(x)
+        # Now we feed in x and paragraph_embedding and hope to get x back
+        recovered_x = self.decoder(paragraph_embedding, x)
+
+        return recovered_x
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters,lr=0.001)
         return optimizer
 
     def training_step(self, batch_inp, step_id):
-        x,y = batch_inp
-        output_pred = self.forward(x)
-        loss = torch.nn.functional.mse_loss(output_pred, y)
-        self.log("train_loss", loss)
-        return loss
+        # Shouldn't be used, use WrappedAutoEncoder instead
+        x = batch_inp
+        outputs = self.forward(x)
+        loss = outputs[0]
+        return {"loss": loss}
+

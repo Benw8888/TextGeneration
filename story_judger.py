5a45e7b89bee8f8b5f96708b45e66ec19f74813f
edwardw21 <kingofgeedorah@gmail.com>
2022-11-29 18:38:59 -0500

    combo story judger preliminary code

--- story_judger.py
+++ story_judger.py
@@ -24,7 +24,6 @@ class BaselineStoryJudger(nn.Module):
 
         lstm_outputs, _ = self.lstm(embedded_x.to(torch.float64))
         return self.linear(lstm_outputs[:,-1])
-        pass
 
 class StoryJudger(nn.Module):
     """
@@ -42,6 +41,38 @@ class StoryJudger(nn.Module):
 
     def forward(self, x):
         paragraph_embeddings = self.encoder(x)
-        pre_output = self.lstm(paragraph_embeddings)
+        pre_output, _ = self.lstm(paragraph_embeddings)
         return self.linear(pre_output[:, -1])
+
+class StoryJudger2(nn.Module):
+    """
+    Try to make a story judger which combines local word-level evaluations with paragraph embedding evaluations?
+    """
+    def __init__(self, encoder): # pass in pre-trained encoer
+        super().__init__()
+        self.encoder = encoder
+        embedding_dim = 512
+        word_hidden_dim = 1000
+        self.embedding = nn.Embedding(num_embeddings=50257, embedding_dim=embedding_dim, dtype = torch.float64)
+        # first lstm for word embeddings
+        self.lstm1 = nn.LSTM(embedding_dim, word_hidden_dim, num_layers=2, bidirectional=False, dtype=torch.float64, batch_first=True)
+        self.linear1 = nn.Linear(word_hidden_dim, 1, dtype=torch.float64)
+        # paragraph embedding evaluation modules
+        # lstm + linear layer
+        self.encoder = encoder
+        paragraph_dim = 768
+        paragraph_hidden_dim = 1000
+        self.lstm2 = nn.LSTM(paragraph_dim, paragraph_hidden_dim, num_layers=2, bidirectional=True, dtype=torch.float64, batch_first=True)
+        self.linear2 = nn.Linear(2*paragraph_hidden_dim, 1, dtype=torch.float64)
+        self.comb_linear = nn.Linear(2, 1, dtype=torch.float64) # final linear layer to combine the word and paragraph scores
+
+    def forward(self, x):
+        paragraph_embeddings = self.encoder(x)
+        paragraph_outputs, _ = self.lstm2(paragraph_embeddings)
+        paragraph_scores = self.linear2(paragraph_outputs[:, -1])
+        embedded_x = self.embedding(x)
+        word_lstm_outputs, _ = self.lstm1(embedded_x.to(torch.float64))
+        word_scores = self.linear1(word_lstm_outputs[:, -1])
+        combo_scores = torch.cat([paragraph_scores, word_scores], -1)
+        return self.comb_linear(combo_scores)
 

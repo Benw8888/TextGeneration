7a80b852a897606a67562973b038c16f3a1bf773
Phire1 <117692814+Phire1@users.noreply.github.com>
2022-12-05 11:52:29 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -43,7 +43,7 @@ class Generator(nn.Module):
     """
 
     # Constructor
-    def __init__(self, dim_model, num_heads, num_encoder_layers, num_decoder_layers, dropout_p, ):
+    def __init__(self, dim_model, num_heads, num_encoder_layers, num_decoder_layers, dropout_p):
         super().__init__()
 
         # INFO
@@ -59,22 +59,18 @@ class Generator(nn.Module):
             nhead=num_heads,
             num_encoder_layers=num_encoder_layers,
             num_decoder_layers=num_decoder_layers,
-            dropout=dropout_p,
+            dropout=dropout_p, batch_first=True
         )
 
     def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
         # Src size must be (batch_size, src sequence length, dim_vectors)
         # Tgt size must be (batch_size, tgt sequence length, dim_vectors)
-
-        # we permute to obtain size (sequence length, batch_size, dim_vectors),
-        src = src.permute(1, 0, 2)
-        tgt = tgt.permute(1, 0, 2)
-
         # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+
         return self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=tgt_pad_mask)
 
     def get_tgt_mask(self, size) -> torch.tensor:
-        # Generates a square matrix where the each row allows one word more to be seen
+        # Generates a square matrix where each row allows one word more to be seen
         mask = torch.tril(torch.ones(size, size) == 1)  # Lower triangular matrix
         mask = mask.float()
         mask = mask.masked_fill(mask == 0, float('-inf'))  # Convert zeros to -inf
@@ -89,7 +85,7 @@ class Generator(nn.Module):
 
         return mask
 
-    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int = -1) -> torch.tensor:
         # If matrix = [1,2,3,0,0,0] where pad_token=0, the result mask is
         # [False, False, False, True, True, True]
         return (matrix == pad_token)

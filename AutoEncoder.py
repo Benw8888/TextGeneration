a84a9e1d876d493857f9daccca6dd6549b43c402
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2022-12-05 22:35:13 -0500

    Update AutoEncoder.py

--- AutoEncoder.py
+++ AutoEncoder.py
@@ -5,6 +5,9 @@ import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 import Encoder
 import Decoder
+import pickle as pickle
+import gzip
+import os
 
 # Implement autoencoder, combining both encoder and decoder
 
@@ -12,14 +15,21 @@ class AutoEncoder(nn.Module):
     """
     Try to make an LSTM encoder
     """
-    def __init__(self):
+    def __init__(self, encoder_file_path = None, decoder_file_path=None):
         super().__init__()
-        # define word embedding module:
-        self.encoder = Encoder.Encoder()
-        self.decoder = Decoder.Decoder()
-        self.decoder_word_embeddings = self.decoder.word_embeddings
+        self.encoder_file_path = encoder_file_path
+        self.decoder_file_path = decoder_file_path
+        if encoder_file_path is not None:
+            # with gzip.open(encoder_file_path, 'rb') as inp:
+            #     self.encoder = pickle.load(inp)
+            self.load_encoder(encoder_file_path)
+        else:
+            self.encoder = Encoder.Encoder()
+        self.decoder = Decoder.Decoder(file_path=decoder_file_path)
 
-    def forward(self,x):
+    def forward(self,input_ids=None, **kwargs):
+        x = input_ids
+        # x is in the format of a list of paragraphs
         paragraph_embedding = self.encoder(x)
         # Now we feed in x and paragraph_embedding and hope to get x back
         recovered_x = self.decoder(paragraph_embedding, x)
@@ -36,4 +46,22 @@ class AutoEncoder(nn.Module):
         outputs = self.forward(x)
         loss = outputs[0]
         return {"loss": loss}
+
+    def save(self, target_folder: str = os.getcwd()):
+        """Saves the model into the specified directory."""
+        self.save_pretrained(target_folder)
+
+    def save_pretrained(self, target_folder: str = os.getcwd()):
+        """Saves the model into the specified directory."""
+        print("SAVING BASED ON: ", target_folder)
+        torch.save(self.encoder.state_dict(), target_folder+"encoder.p")
+        # with gzip.open(target_folder+"encoder.p", 'wb') as outp:
+        #     pickle.dump(self.encoder, outp, -1)
+        print("saving decoder!")
+        self.decoder.save_pretrained(target_folder+"decoder")
+
+    def load_encoder(self, path):
+        self.encoder.load_state_dict(torch.load(path))
+
+
 

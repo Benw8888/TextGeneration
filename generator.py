8ea304b8c54322abb2f9794e4edcb09f776ad6bb
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2022-12-15 23:44:53 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -18,36 +18,6 @@ import Encoder
 import Decoder
 
 
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, dim_model, dropout_p, max_len):
-        super().__init__()
-        # Modified version from: https://pytorch.org/tutorials/beginner/transformer_tutorial.html
-        # max_len determines how far the position can have an effect on a token (window)
-
-        # Info
-        self.dropout = nn.Dropout(dropout_p)
-
-        # Encoding - From formula
-        pos_encoding = torch.zeros(max_len, dim_model)
-        positions_list = torch.arange(0, max_len, dtype=torch.float).view(-1, 1)  # 0, 1, 2, 3, 4, 5
-        division_term = torch.exp(
-            torch.arange(0, dim_model, 2).float() * (-math.log(10000.0)) / dim_model)  # 1000^(2i/dim_model)
-
-        # PE(pos, 2i) = sin(pos/1000^(2i/dim_model))
-        pos_encoding[:, 0::2] = torch.sin(positions_list * division_term)
-
-        # PE(pos, 2i + 1) = cos(pos/1000^(2i/dim_model))
-        pos_encoding[:, 1::2] = torch.cos(positions_list * division_term)
-
-        # Saving buffer (same as parameter without gradients needed)
-        pos_encoding = pos_encoding.unsqueeze(0).transpose(0, 1)
-        self.register_buffer("pos_encoding", pos_encoding)
-
-    def forward(self, token_embedding: torch.tensor) -> torch.tensor:
-        # Residual connection + pos encoding
-        return self.dropout(token_embedding + self.pos_encoding[:token_embedding.size(0), :])
-
 class TextGenerator(nn.Module):
     def __init__(self, generator_file_path, decoder_file_path, size_input=30, device=None):
         super().__init__()
@@ -83,12 +53,18 @@ class TextGenerator(nn.Module):
 
         # now expand each chunk vector into tokens/words
 
+        text = self.decode_chunks(chunk_vectors)
+
 
 
         pass
 
     def create_start_vectors(self, batch_size):
         return torch.zeros((1, 1, 768))
+
+    def decode_chunks(self):
+
+        pass
 
     def generate_all_chunks(self, shifted_input):
         # do one step generation, return past value keys somehow
